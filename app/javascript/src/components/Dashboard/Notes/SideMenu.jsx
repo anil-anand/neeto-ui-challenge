5fd79dfa3255f2cbef3f31ef3adc187cac10f76a
Anil Anand <anilanandchess@gmail.com>
2022-09-28 21:04:06 +0530

    Made corrections to SideMenu

--- app/javascript/src/components/Dashboard/Notes/SideMenu.jsx
+++ app/javascript/src/components/Dashboard/Notes/SideMenu.jsx
@@ -5,58 +5,56 @@ import { Typography } from "neetoui";
 import { MenuBar } from "neetoui/layouts";
 
 const SideMenu = ({ showMenu }) => (
-  <div className="flex">
-    <MenuBar showMenu={showMenu} title="Notes">
-      <MenuBar.Block active count={200} label="All" />
-      <MenuBar.Block count={80} label="Users" />
-      <MenuBar.Block count={60} label="Leads" />
-      <MenuBar.Block count={60} label="Visitors" />
-      <MenuBar.SubTitle
-        iconProps={[
-          {
-            icon: Search,
-          },
-        ]}
+  <MenuBar showMenu={showMenu} title="Notes">
+    <MenuBar.Block active count={200} label="All" />
+    <MenuBar.Block count={80} label="Users" />
+    <MenuBar.Block count={60} label="Leads" />
+    <MenuBar.Block count={60} label="Visitors" />
+    <MenuBar.SubTitle
+      iconProps={[
+        {
+          icon: Search,
+        },
+      ]}
+    >
+      <Typography
+        component="h4"
+        style="h5"
+        textTransform="uppercase"
+        weight="bold"
       >
-        <Typography
-          component="h4"
-          style="h5"
-          textTransform="uppercase"
-          weight="bold"
-        >
-          Segments
-        </Typography>
-      </MenuBar.SubTitle>
-      <MenuBar.Block count={80} label="Europe" />
-      <MenuBar.Block count={60} label="Middle-East" />
-      <MenuBar.Block count={60} label="Asia" />
-      <MenuBar.SubTitle
-        iconProps={[
-          {
-            icon: Search,
-          },
-          {
-            icon: Plus,
-          },
-          {
-            icon: Settings,
-          },
-        ]}
+        Segments
+      </Typography>
+    </MenuBar.SubTitle>
+    <MenuBar.Block count={80} label="Europe" />
+    <MenuBar.Block count={60} label="Middle-East" />
+    <MenuBar.Block count={60} label="Asia" />
+    <MenuBar.SubTitle
+      iconProps={[
+        {
+          icon: Search,
+        },
+        {
+          icon: Plus,
+        },
+        {
+          icon: Settings,
+        },
+      ]}
+    >
+      <Typography
+        component="h4"
+        style="h5"
+        textTransform="uppercase"
+        weight="bold"
       >
-        <Typography
-          component="h4"
-          style="h5"
-          textTransform="uppercase"
-          weight="bold"
-        >
-          Tags
-        </Typography>
-      </MenuBar.SubTitle>
-      <MenuBar.Block count={80} label="Sales" />
-      <MenuBar.Block count={60} label="Finance" />
-      <MenuBar.Block count={60} label="User Experience" />
-    </MenuBar>
-  </div>
+        Tags
+      </Typography>
+    </MenuBar.SubTitle>
+    <MenuBar.Block count={80} label="Sales" />
+    <MenuBar.Block count={60} label="Finance" />
+    <MenuBar.Block count={60} label="User Experience" />
+  </MenuBar>
 );
 
 export default SideMenu;
